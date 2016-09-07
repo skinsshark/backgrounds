ff734c9950d7927389eeea57f154245645689095
szheng <sharon.zheng@live.com>
2016-09-07 19:08:36 -0400

    update header

--- src/resources/js/controllers/MainController.js
+++ src/resources/js/controllers/MainController.js
@@ -1,34 +1,34 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Top Sellers in Books';
-  $scope.promo = 'asdf';
+  $scope.title = 'Backgrounds';
+  $scope.desc = 'a collection of photographs i took that i like to use as backgrounds';
   $scope.photos = [ 
   { 
-    name: 'The Book of Trees', 
+    name: 'Cardwell, Montana', 
     price: 19, 
     pubdate: new Date('2014', '03', '08'), 
     cover: '../../src/resources/images/test.jpg' ,
     likes: 0
   }, 
   { 
-    name: 'Program or be Programmed', 
+    name: 'West Yellowstone, Montana', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
     cover: '../../src/resources/images/tester.jpg' ,
     likes: 0
   },
   {
-    name: 'Program or be Programmed', 
+    name: 'Vancouver, British Columbia', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
     cover: 'img/program-or-be-programmed.jpg',
     likes: 0 
-  },
-  {
-    name: 'Program or be Programmed', 
-    price: 8, 
-    pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg',
-    likes: 0 
-  }
+  } // ,
+  // {
+  //   name: 'Program or be Programmed', 
+  //   price: 8, 
+  //   pubdate: new Date('2013', '08', '01'), 
+  //   cover: 'img/program-or-be-programmed.jpg',
+  //   likes: 0 
+  // }
 ];
 }]); 
