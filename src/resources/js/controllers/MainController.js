146ce38ddafff917cf7f3ce761217c53938b6007
szheng <sharon.zheng@live.com>
2016-09-08 17:42:27 -0400

    fix image urls

--- src/resources/js/controllers/MainController.js
+++ src/resources/js/controllers/MainController.js
@@ -4,23 +4,23 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.photos = [ 
   { 
     name: 'Cardwell, Montana', 
-    price: 19, 
-    pubdate: new Date('2014', '03', '08'), 
-    cover: '../../src/resources/images/test.jpg' ,
+    // price: 19, 
+    // pubdate: new Date('2014', '03', '08'), 
+    cover: '/src/resources/images/test.jpg' ,
     likes: 0
   }, 
   { 
     name: 'West Yellowstone, Montana', 
-    price: 8, 
-    pubdate: new Date('2013', '08', '01'), 
-    cover: '../../src/resources/images/tester.jpg' ,
+    // price: 8, 
+    // pubdate: new Date('2013', '08', '01'), 
+    cover: '/src/resources/images/tester.jpg' ,
     likes: 0
   },
   {
     name: 'Vancouver, British Columbia', 
-    price: 8, 
-    pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg',
+    // price: 8, 
+    // pubdate: new Date('2013', '08', '01'), 
+    cover: '/src/resources/images/test.jpg',
     likes: 0 
   } // ,
   // {
