b5fbddf8bb9cbc2c8db6720673d971581b17878a
szheng <sharon.zheng@live.com>
2016-09-11 21:07:47 -0400

    update header and new screenshots

--- src/resources/js/controllers/MainController.js
+++ src/resources/js/controllers/MainController.js
@@ -1,20 +1,21 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Backgrounds';
   $scope.desc = 'a collection of photographs i took that i like to use as backgrounds';
+  $scope.instruc = 'click on an image to open it up in a new tab to save the image';
   $scope.photos = [ 
   { 
     name: 'Cardwell, Montana', 
     // price: 19, 
     // pubdate: new Date('2014', '03', '08'), 
-    cover: '/src/resources/images/test.jpg' ,
-    likes: 0
+    cover: '/src/resources/images/cardwell-demo.png' ,
+    likes: 'https://github.com/skinsshark/Backgrounds/raw/master/src/resources/images/cardwell.jpg'
   }, 
   { 
     name: 'West Yellowstone, Montana', 
     // price: 8, 
     // pubdate: new Date('2013', '08', '01'), 
     cover: '/src/resources/images/tester.jpg' ,
-    likes: 0
+    link: 2
   },
   {
     name: 'Vancouver, British Columbia', 
