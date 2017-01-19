5d99e69b6faa348023e13fdf28bdafe80c2dab65
sharon zheng <sharon.zheng@live.com>
2017-01-19 01:30:06 -0500

    new urls

--- resources/js/controllers/MainController.js
+++ resources/js/controllers/MainController.js
@@ -5,38 +5,38 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.photos = [
     {
       name: 'Yellowstone, Probably',
-      cover: '/Backgrounds/resources/images/yellowstone-demo.png',
+      cover: '/resources/images/yellowstone-demo.png',
       link:'yellowstone.jpg'
 
     },
     {
       name: 'West Yellowstone, Montana',
-      cover: '/Backgrounds/resources/images/wyellowstone-demo.png',
+      cover: '/resources/images/wyellowstone-demo.png',
       link: 'wyellowstone.jpg'
     },
     {
       name: 'Cardwell, Montana',
-      cover: '/Backgrounds/resources/images/cardwell-demo.png',
+      cover: '/resources/images/cardwell-demo.png',
       link: 'cardwell.jpg'
     },
     {
       name: 'Norris Geyser Basin, Wyoming',
-      cover: '/Backgrounds/resources/images/norris-geyser-demo.png',
+      cover: '/resources/images/norris-geyser-demo.png',
       link:'norris-geyser.jpg'
     },
     {
       name: 'Hardwood Lookout Trail, Algonquin Park',
-      cover: '/Backgrounds/resources/images/hardwood-lookout-demo.png',
+      cover: '/resources/images/hardwood-lookout-demo.png',
       link: 'hardwood-lookout.jpeg'
     },
     {
       name: 'Vancouver, British Columbia',
-      cover: '/Backgrounds/resources/images/vancouver-demo.png',
+      cover: '/resources/images/vancouver-demo.png',
       link: 'vancouver.jpg'
     },
     {
       name: 'Aga Khan Museum, Toronto',
-      cover: '/Backgrounds/resources/images/aga-khan-demo.png',
+      cover: '/resources/images/aga-khan-demo.png',
       link: 'aga-khan.jpg'
     }];
   }]);
