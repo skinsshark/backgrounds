4f8d02205671de656c85893618c32d1d400cbded
Sharon Zheng <sharzheng@gmail.com>
2023-04-06 22:20:04 -0700

    update image urls

--- resources/js/controllers/MainController.js
+++ resources/js/controllers/MainController.js
@@ -5,38 +5,38 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.photos = [
     {
       name: 'Yellowstone, Probably',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/yellowstone-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/yellowstone-demo.png',
       link:'yellowstone.jpg'
 
     },
     {
       name: 'West Yellowstone, Montana',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/wyellowstone-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/wyellowstone-demo.png',
       link: 'wyellowstone.jpg'
     },
     {
       name: 'Cardwell, Montana',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/cardwell-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/cardwell-demo.png',
       link: 'cardwell.jpg'
     },
     {
       name: 'Norris Geyser Basin, Wyoming',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/norris-geyser-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/norris-geyser-demo.png',
       link:'norris-geyser.jpg'
     },
     {
       name: 'Hardwood Lookout Trail, Algonquin Park',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/hardwood-lookout-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/hardwood-lookout-demo.png',
       link: 'hardwood-lookout.jpeg'
     },
     {
       name: 'Vancouver, British Columbia',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/vancouver-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/vancouver-demo.png',
       link: 'vancouver.jpg'
     },
     {
       name: 'Aga Khan Museum, Toronto',
-      cover: 'https://github.com/skinsshark/backgrounds/blob/master/resources/images/aga-khan-demo.png',
+      cover: 'https://raw.githubusercontent.com/skinsshark/backgrounds/master/resources/images/aga-khan-demo.png',
       link: 'aga-khan.jpg'
     }];
   }]);
